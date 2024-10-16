b9988b0bb0d9aa5796465712367f186cfd1d5c67
JJambros <143129381+JJambros@users.noreply.github.com>
2024-10-16 17:01:19 -0400

    integrated prototype pages into angular

--- stockit/src/app/app.routes.ts
+++ stockit/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LoginLayoutComponent, // No navbar for login layout
+        children: [
+          { path: 'login', component: LoginComponent }
+        ]
+      },
+      {
+        path: '',
+        component: MainLayoutComponent, // Navbar for main layout
+        children: [
+          { path: 'dashboard', component: DashboardComponent },
+          { path: 'inventory', component: InventoryComponent },
+          { path: 'profile', component: ProfileComponent }
+        ]
+      },
+      { path: '**', redirectTo: 'login' }  // Redirect to login if unknown route
+];
