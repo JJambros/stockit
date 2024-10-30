83a4a5b32ce0b7580de98294facddad1dca6681d
Priyanka Shah <priyanka2003shah@gmail.com>
2024-10-30 17:55:18 -0400

    add order/inventory/audits

--- stockit/src/app/inventory/inventory.component.ts
+++ stockit/src/app/inventory/inventory.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MyDataService } from '../my-data.service';
+import { CommonModule } from '@angular/common';
+import { error } from 'console';
 @Component({
   selector: 'app-inventory',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inventory.component.html',
-  styleUrl: './inventory.component.css'
+  styleUrls: ['./inventory.component.css'],
+  providers: [MyDataService]
 })
-export class InventoryComponent {
+export class InventoryComponent implements OnInit  {
+ inventoryList: any[] =[];
 
+ constructor(private myDataService: MyDataService){}
+
+ ngOnInit(): void {
+   this.myDataService.getInventory().subscribe(
+    (data) => {
+      console.log('got inventory data', data);
+      this.inventoryList = Array.isArray(data) ? data : [];
+    },
+    (error) => console.error('error fetching INVENTORY data', error)
+   );
+ }
 }
