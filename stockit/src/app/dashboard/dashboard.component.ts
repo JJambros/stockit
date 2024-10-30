e61b90dadef4b059075d02ff608e2775eaf3fa5c
JJambros <143129381+JJambros@users.noreply.github.com>
2024-10-30 19:55:45 -0400

    added sales, orders functionality for dashboard

--- stockit/src/app/dashboard/dashboard.component.ts
+++ stockit/src/app/dashboard/dashboard.component.ts
@@ -10,12 +10,22 @@ import { MyDataService } from '../my-data.service';
 })
 export class DashboardComponent implements OnInit {
   profile: any;
+  orders: any;
+  sales: any;
 
   constructor(private dataService: MyDataService) { }
   
   ngOnInit(): void {
     this.dataService.getUserProfile().subscribe(data => {
       this.profile = data;
+    });
+
+    this.dataService.getDashboardOrders('24h').subscribe(data => {
+      this.orders = data;
+    });
+
+    this.dataService.getDashboardNetSales('24h').subscribe(data => {
+      this.sales = data;
     });
   }
 
