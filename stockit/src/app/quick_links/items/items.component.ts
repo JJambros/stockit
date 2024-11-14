c8b675897845f72c2868c90a0a107c474f1cdbfb
JJambros <143129381+JJambros@users.noreply.github.com>
2024-11-14 15:48:25 -0500

    feat: update and delete in item list

--- stockit/src/app/quick_links/items/items.component.ts
+++ stockit/src/app/quick_links/items/items.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MyDataService } from '../../my-data.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-items',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './items.component.html',
   styleUrl: './items.component.css',
   providers: [MyDataService]
@@ -13,7 +14,8 @@ import { MyDataService } from '../../my-data.service';
 export class ItemsComponent implements OnInit {
   itemsList: any[]=[];
   constructor(private myDataService: MyDataService){}
-ngOnInit(): void {
+
+  ngOnInit(): void {
   this.myDataService.getInventory().subscribe(
     (data) => {
       console.log('got inventory data', data);
@@ -21,5 +23,29 @@ ngOnInit(): void {
     }, 
     (error) => console.error('error fetching data', error)
   );
-}
+  }
+
+  deleteItem(id: number){
+    if(confirm('Are you sure you want to delete this item from inventory?')){
+      this.myDataService.softDeleteItems(id).subscribe(
+      (data) => {
+        console.log('deleted item', data);
+        this.itemsList = this.itemsList.filter((item) => item.inventory_id !== id);
+      },
+      (error) => console.error('error deleting item', error)
+    );
+    }
+  }
+
+  updateItem(item: any) {
+    if(confirm('Are you sure you want to update this item?')){
+      this.myDataService.updateInventoryItem(item).subscribe(
+      (data) => {
+        console.log('Updated item:', data);
+        // Optionally refresh the item in itemsList or display a success message
+      },
+      (error) => console.error('Error updating item:', error)
+    );
+    }
+  }
 }
